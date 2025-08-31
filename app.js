97c28ec0844eed99fbbd2513488f5863056c2d8d
Prateek Gokhale <prateekgokhale35@gmail.com>
2025-08-31 16:28:05 +0530

    fix: use process.env.PORT instead of hardcoded 8080

--- app.js
+++ app.js
@@ -123,6 +123,7 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
